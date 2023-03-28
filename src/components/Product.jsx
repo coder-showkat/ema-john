99d988365585a71d00a6134bb1c4eb1ae3412672
coder-showkat <showkat.coder@gmail.com>
2023-03-28 15:17:38 +0600

    added intersaction observer

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,55 +5,49 @@ import "./Product.css";
 
 export default function Product({ item, addToCartHandler }) {
   const { id, img, name, price, seller, ratings } = item;
-  const [shouldLoad, setShouldLoad] = useState(false);
-  const divRef = useRef(null);
+  const [shouldLoadImage, setShouldLoadImage] = useState(false);
+  const imageRef = useRef(null);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
-          setShouldLoad(true);
+          setShouldLoadImage(true);
           observer.disconnect();
         }
       },
       { threshold: 0.3 }
     );
-    if (divRef.current) {
-      observer.observe(divRef.current);
+    if (imageRef.current) {
+      observer.observe(imageRef.current);
     }
 
     return () => {
-      if (divRef.current) {
-        observer.unobserve(divRef.current);
+      if (imageRef.current) {
+        observer.unobserve(imageRef.current);
       }
     };
   }, []);
 
   return (
-    <div ref={divRef} className="product">
-      {shouldLoad ? (
-        <>
-          <div className="card-header">
-            <img src={img} alt={name} />
-            <h2>{name}</h2>
-            <h4>Price: ${price}</h4>
-          </div>
-          <div className="card-body">
-            <p>Manufacturer: {seller}</p>
-            <p>Rating: {ratings} star</p>
-            <button
-              onClick={() => {
-                addToCartHandler(id);
-                ToastSuccess(name + " is added to cart!");
-              }}
-            >
-              Add to Cart <ion-icon name="cart-outline"></ion-icon>
-            </button>
-          </div>
-        </>
-      ) : (
-        <img src={spinner} />
-      )}
+    <div className="product">
+      <div className="card-header">
+        <img ref={imageRef} src={shouldLoadImage ? img : spinner} alt={name} />
+        <h2>{name}</h2>
+        <h4>Price: ${price}</h4>
+      </div>
+      <div className="card-body">
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings} star</p>
+        <button
+          onClick={() => {
+            addToCartHandler(id);
+            ToastSuccess(name + " is added to cart!");
+          }}
+        >
+          Add to Cart <ion-icon name="cart-outline"></ion-icon>
+        </button>
+      </div>
     </div>
   );
 }
